3d9353baa9a73e68e16c550092c20e9d01ba2800
chp_h <veron.grigoreva@gmail.com>
2024-04-03 20:26:04 +0300

    Edit ClientCode.java

--- ClientCode.java
+++ ClientCode.java
@@ -8,8 +8,7 @@ public class ClientCode{
 
     
             List<Product> productList = new ArrayList<>();
-    
-    
+        
             productList.add(new HotDrink(90, "tea", 200, 60));
             productList.add(new HotDrink(100, "espresso", 50, 70));
             productList.add(new HotDrink(150, "cocoa", 300, 60));
@@ -20,6 +19,16 @@ public class ClientCode{
     
             System.out.println(vendingMachine.getProduct("cappuccino", 350, 75));
     
+            List<Product> productList1 = new ArrayList<>();
+            productList1.add(new HotDrink(90, "icetea", 200, 18));
+            productList1.add(new HotDrink(100, "water", 200, 22));
+            productList1.add(new HotDrink(150, "applejuice", 200, 22));
+            productList1.add(new HotDrink(150, "frape", 300, 18));
+    
+            HotDrinksMachine vendingMachine1 = new HotDrinksMachine();
+            vendingMachine1.initProduct(productList1);
+    
+            System.out.println(vendingMachine1.getProduct("water", 200, 22));
             
         }
     }
